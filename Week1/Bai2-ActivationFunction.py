cfc4df96d43b877606ac72cc9b9fe64125830cff
tiephn <vc.tiepdoviet@gmail.com>
2024-06-25 20:31:38 +0700

    update

--- Week1/Bai2-ActivationFunction.py
+++ Week1/Bai2-ActivationFunction.py
@@ -1,46 +1,51 @@
 
 import math
 
+
 def is_number(n):
-  try:
-    float (n)
-  except ValueError:
-    return False
-  return True
+    try:
+        float(n)
+    except ValueError:
+        return False
+    return True
+
 
 def sigmoid(x):
-  return 1/(1+math.exp(-x))
+    return 1/(1+math.exp(-x))
+
 
 def relu(x):
-  if x<=0:
-    return 0
-  else:
-    return xs
+    if x <= 0:
+        return 0
+    else:
+        return x
 
-def elu(x,alpha=0.01):
-  if x>0:
-    return x
-  else:
-    return alpha*(math.exp(x)-1)
+
+def elu(x, alpha=0.01):
+    if x > 0:
+        return x
+    else:
+        return alpha*(math.exp(x)-1)
+
 
 def activation_function(x, act_fun):
-  if act_fun == 'sigmoid':
-    print(f"sigmoid: f({x}) = {sigmoid(x)}")
-  elif act_fun == 'relu':
-    print(f"ReLU: f({x}) = {relu(x)}")
-  elif act_fun == 'elu':
-    print(f"ELU: f({x}) = {elu(x)}")
-  else:
-    print(f"{act_fun} is not supported")
+    if act_fun == 'sigmoid':
+        print(f"sigmoid: f({x}) = {sigmoid(x)}")
+    elif act_fun == 'relu':
+        print(f"ReLU: f({x}) = {relu(x)}")
+    elif act_fun == 'elu':
+        print(f"ELU: f({x}) = {elu(x)}")
+    else:
+        print(f"{act_fun} is not supported")
 
 
 def exercise2():
-  x = input("Input x:")
-  if is_number(x):
-    act_fun = input("Input activation Function ( sigmoid | relu | elu ):")
-    activation_function(float(x),act_fun)
-  else:
-    print('x must be a number')
+    x = input("Input x:")
+    if is_number(x):
+        act_fun = input("Input activation Function ( sigmoid | relu | elu ):")
+        activation_function(float(x), act_fun)
+    else:
+        print('x must be a number')
 
 
 exercise2()
